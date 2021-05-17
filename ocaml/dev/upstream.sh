51a558eeaf79b382ffbc70fed46a03852fd82c87
Vladimir N. Silyaev <TheCBaH@gmail.com>
2021-05-16 18:14:44 -0700

    fixup! Added script for upstreaming

--- ocaml/dev/upstream.sh
+++ ocaml/dev/upstream.sh
@@ -2,10 +2,13 @@
 set -eu
 set -x
 base=$(git merge-base HEAD remotes/flatbuffers/master)
-commit=86533fc32d23a6e9b7dba71df6e7e7d16b597572
+commit=847da5734c6addd67b5f933a036b20440d15f6a0
 git diff $base HEAD >.ocaml.patch
+version=0
 echo 'git branch -D pullrequest.ocaml' >.ocaml.branch
 echo 'git checkout -b pullrequest.ocaml flatbuffers/master' >>.ocaml.branch
 echo 'git apply --exclude=.vscode/\* --exclude=ocaml/dev/\* --verbose --check  .ocaml.patch' >.ocaml.apply
 echo 'git apply --exclude=.vscode/\* --exclude=ocaml/dev/\* --index .ocaml.patch' >>.ocaml.apply
 echo "git commit -C $commit" >>.ocaml.apply
+echo 'git push origin +ocaml_support.pr' > .ocaml.push
+echo "git push origin +ocaml_support.pr.v$version" >> .ocaml.push
